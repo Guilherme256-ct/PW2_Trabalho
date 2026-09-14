b39a00ffbf399ad723b387415841af4cb94d1a4f
Guilherme Giovanini <guilhermegiovanini1403@gmail.com>
2026-09-14 17:45:28 -0300

    feat: fazendo alterações relacionadas a datas no script.js

--- html/aplicação-do-dia-que-nasceu/script.js
+++ html/aplicação-do-dia-que-nasceu/script.js
@@ -3,8 +3,13 @@ function Exibir(){
     var mes=document.getElementById("mes_nasc").value;
     var ano=document.getElementById("ano_nasc").value;
     var hoje= new Date();
+
+    var dia_hoje= hoje.getDate();
+    var mes_hoje = hoje.getMonth()+1;
+    var ano_hoje = hoje.getFullYear();
+
     var mesExtenso=new Array("Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Desembro");
-    
+    var diaExtenso=new Array("Domingo","Segunda","Terça","Quarta","Quinta","Sexta","Sábado");
     //Checa o dia
     if(isNaN(dia) || dia==""){
         alert("Forneça um dia válido!");
@@ -25,31 +30,45 @@ function Exibir(){
         document.getElementById("ano_nasc").focus();
     }
     else{
-        dia=parseFloat(dia);
-        mes=parseFloat(mes);
-        ano=parseFloat(ano);
+        dia=parseInt(dia);
+        mes=parseInt(mes-1);
+        ano=eval(ano);
+        ano=parseInt(ano);
         
+        hoje.setDate(dia);
+        hoje.setMonth(mes);
+        hoje.setFullYear(ano);
+
         //checa se o ano, mes e dia são inteiros e se estão corretos
-        if(ano%1!=0){
-            alert("Forneça um ano válido que seja INTEIRO!");
-            document.getElementById("ano_nasc").focus();
-        }
-        else if(mes%1!=0 || mes>12 || mes<1){
+        
+        if(mes>12 || mes<1){
             alert("Forneça um mês válido que seja INTEIRO e entre 1 e 12!");
             document.getElementById("mes_nasc").focus();
         }
-        else if(dia%1!=0 || dia>31 || dia<1){
+        else if(dia>31 || dia<1){
             alert("Forneça um dia válido que seja INTEIRO e entre 1 e 31!");
             document.getElementById("dia_nasc").focus(); 
         }
         else{
-            
-            ano=eval(ano);
-            ano=parseInt(ano);
-            mes=parseInt(mes)-1;
-            dia=parseInt(dia);
-            
             document.getElementById("mes_texto").value=mesExtenso[mes];
+            var semana=dia%7;
+            if(semana==0){
+                semana=7;
+            }
+            document.getElementById("dia_semana").value=diaExtenso[hoje.getDay()]
+            var idadePessoa=hoje.getFullYear()-ano;
+            if(hoje.getFullYear()==ano){
+                if(hoje.getMonth()<mes){
+                    idadePessoa-=1;
+                }
+                else if(hoje.getMonth()==mes){
+                    if(hoje.getDate()>dia){
+                        idadePessoa-=1;
+                    }
+                }
+            }
+            document.getElementById("idade").value=idadePessoa;
+                                            
             
         }
     }
